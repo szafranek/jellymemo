08c22371b0f65cb5765c9dc06c81006cd34214b3
Krzysztof Szafranek <ebraun@o2.pl>
2015-01-25 22:37:54 +0100

    Consistent formatting

--- js/loader.js
+++ js/loader.js
@@ -1,83 +1,85 @@
 var App = App || {};
 (function() {
-    "use strict";
+	"use strict";
 
-    /** Module definition */
-    App.loadFiles = function (sourceLang, targetLang, callback) {
-        var state = new LoadState();
-        state.toLoad.push(sourceLang);
-        state.toLoad.push(targetLang);
-        loadFile(sourceLang, state, 0, callback);
-        loadFile(targetLang, state, 1, callback);
-    };
+	/** Module definition */
+	App.loadFiles = function(sourceLang, targetLang, callback) {
+		var state = new LoadState();
+		state.toLoad.push(sourceLang);
+		state.toLoad.push(targetLang);
+		loadFile(sourceLang, state, 0, callback);
+		loadFile(targetLang, state, 1, callback);
+	};
 
-    /** Functions */
-    function lineToArray(line) {
-        if (!line.match(/".*"\s=\s".*";/i)) {
-            return null;
-        }
-        var arr = line.split("\" = \"");
-        arr[0] = arr[0].substring(1);
-        arr[1] = arr[1].slice(0, -2);
-        return arr;
-    }
+	/** Functions */
+	function lineToArray(line) {
+		if (!line.match(/".*"\s=\s".*";/i)) {
+			return null;
+		}
+		var arr = line.split("\" = \"");
+		arr[0] = arr[0].substring(1);
+		arr[1] = arr[1].slice(0, -2);
+		return arr;
+	}
 
-    function mergeLangs(langs) {
-        var i, j;
-        var result = [];
-        var linesFirst = langs[0].split("\n");
-        var linesSecond = langs[1].split("\n");
-        for (i = 0; i < linesFirst.length; i++) {
-            var lineFirst = lineToArray(linesFirst[i]);
-            if (!lineFirst) {
-                continue;
-            }
-            var keyFirst = lineFirst[0];
-            var valFirst = lineFirst[1];
-            if (valFirst.length <= 1) {
-                continue;
-            }
-            for (j = 0; j < linesSecond.length; j++) {
-                var lineSecond = lineToArray(linesSecond[j]);
-                if (!lineSecond) {
-                    continue;
-                }
-                var keySecond = lineSecond[0];
-                var valSecond = lineSecond[1];
-                if (keyFirst === keySecond && valSecond.length > 1) {
-                    result.push([valFirst, valSecond]);
-                    // Optimization: remove matched element to reduce lookups
-                    linesSecond.splice(j, 1);
-                }
-            }
-        }
-        return result;
-    }
+	function mergeLangs(langs) {
+		var i, j;
+		var result = [];
+		var linesFirst = langs[0].split("\n");
+		var linesSecond = langs[1].split("\n");
+		for (i = 0; i < linesFirst.length; i++) {
+			var lineFirst = lineToArray(linesFirst[i]);
+			if (!lineFirst) {
+				continue;
+			}
+			var keyFirst = lineFirst[0];
+			var valFirst = lineFirst[1];
+			if (valFirst.length <= 1) {
+				continue;
+			}
+			for (j = 0; j < linesSecond.length; j++) {
+				var lineSecond = lineToArray(linesSecond[j]);
+				if (!lineSecond) {
+					continue;
+				}
+				var keySecond = lineSecond[0];
+				var valSecond = lineSecond[1];
+				if (keyFirst === keySecond && valSecond.length > 1) {
+					result.push([valFirst, valSecond]);
+					// Optimization: remove matched element to reduce lookups
+					linesSecond.splice(j, 1);
+				}
+			}
+		}
+		return result;
+	}
 
-    function loadFile(fileName, loadState, resultIndex, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', fileName, true);
-        xhr.onload = function() {
-            if (xhr.status >= 200 && xhr.status < 400) {
-                loadState.loaded.push(fileName);
-                loadState.results[resultIndex] = xhr.responseText.trim();
-                if (loadState.isComplete()) {
-                    var merged = mergeLangs(loadState.results);
-                    callback(merged);
-                }
-            }
-        };
-        xhr.onerror = function() {};
-        xhr.send();
-    }
+	function loadFile(fileName, loadState, resultIndex, callback) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', fileName, true);
+		xhr.onload = function() {
+			if (xhr.status >= 200 && xhr.status < 400) {
+				loadState.loaded.push(fileName);
+				loadState.results[resultIndex] = xhr.responseText.trim();
+				if (loadState.isComplete()) {
+					var merged = mergeLangs(loadState.results);
+					callback(merged);
+				}
+			}
+		};
+		xhr.onerror = function() {
+		};
+		xhr.send();
+	}
 
-    function LoadState() {
-        this.results = [];
-        this.toLoad = [];
-        this.loaded = [];
-    }
-    LoadState.prototype.isComplete = function() {
-        return this.loaded.length === this.toLoad.length;
-    };
+	function LoadState() {
+		this.results = [];
+		this.toLoad = [];
+		this.loaded = [];
+	}
+
+	LoadState.prototype.isComplete = function() {
+		return this.loaded.length === this.toLoad.length;
+	};
 
 }());
--- js/main.js
+++ js/main.js
@@ -32,17 +32,17 @@
 			event.preventDefault();
 		}, false);
 	}
-	
+
 	function isSpeechOn() {
 		return window.speechSynthesis && SpeechSynthesisUtterance !== "undefined";
 	}
 
 	function getRandomPhrase() {
-        var phrases = d.querySelectorAll(".phrase");
+		var phrases = d.querySelectorAll(".phrase");
 		var randomIndex = Math.floor(Math.random() * (phrases.length));
 		return phrases[randomIndex];
 	}
-	
+
 	function setScore(knownScore, totalScore) {
 		d.querySelector(".hud").innerHTML = knownScore + " / " + totalScore;
 	}
@@ -52,27 +52,27 @@
 		return score;
 	}
 
-    function renderPhrases(data) {
-        var templateString = d.querySelector(".phrase-template").innerHTML;
-        var templateParent = d.createElement("div");
-        templateParent.innerHTML = templateString;
-        var template = templateParent.children[0];
+	function renderPhrases(data) {
+		var templateString = d.querySelector(".phrase-template").innerHTML;
+		var templateParent = d.createElement("div");
+		templateParent.innerHTML = templateString;
+		var template = templateParent.children[0];
 
-        var fragment = d.createDocumentFragment();
-        var i, element, string;
-        for (i = 0; i < data.length; i++) {
-            element = template.cloneNode(true);
-            string = element.innerHTML;
-            string = string.replace("{{source}}", data[i][0]);
-            string = string.replace("{{target}}", data[i][1]);
-            element.innerHTML = string;
-            fragment.appendChild(element);
-        }
-        d.querySelector(".content").appendChild(fragment);
-    }
+		var fragment = d.createDocumentFragment();
+		var i, element, string;
+		for (i = 0; i < data.length; i++) {
+			element = template.cloneNode(true);
+			string = element.innerHTML;
+			string = string.replace("{{source}}", data[i][0]);
+			string = string.replace("{{target}}", data[i][1]);
+			element.innerHTML = string;
+			fragment.appendChild(element);
+		}
+		d.querySelector(".content").appendChild(fragment);
+	}
 
-    /** Event listeners */
-    var speakButton = d.querySelector(".actions .speak");
+	/** Event listeners */
+	var speakButton = d.querySelector(".actions .speak");
 	speakButton.addEventListener(Util.touchEvent, function speak(event) {
 		var phrase = d.querySelector(".phrase.current");
 		var button = event.currentTarget;
@@ -93,7 +93,7 @@
 			window.speechSynthesis.speak(utterance);
 		}
 	});
-	
+
 	var actionButtons = d.querySelectorAll(".actions .known, .actions .unknown");
 	Util.addEventListener(actionButtons, Util.touchEvent, function markAnswer(event) {
 		var known = event.currentTarget.classList.contains("known");
@@ -104,7 +104,7 @@
 		if (known) {
 			++current;
 		}
-        setScore(current, ++total);
+		setScore(current, ++total);
 
 		var phrase = d.querySelector(".phrase.current");
 		phrase.classList.add("done");
@@ -115,16 +115,16 @@
 		randomPhrase.classList.add("next");
 
 		setTimeout((function(donePhrase, nextPhrase) {
-            return function() {
-                if (isSpeechOn()) {
-                    window.speechSynthesis.cancel();
-                }
-                donePhrase.classList.remove("done");
-                donePhrase.classList.remove(resultClass);
-                nextPhrase.classList.remove("next");
-                nextPhrase.classList.add("current");
-            };
-        }(phrase, randomPhrase)), 300);
+			return function() {
+				if (isSpeechOn()) {
+					window.speechSynthesis.cancel();
+				}
+				donePhrase.classList.remove("done");
+				donePhrase.classList.remove(resultClass);
+				nextPhrase.classList.remove("next");
+				nextPhrase.classList.add("current");
+			};
+		}(phrase, randomPhrase)), 300);
 	}, false);
 
 	var content = d.querySelector(".content");
